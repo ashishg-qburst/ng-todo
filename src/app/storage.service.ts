f9fc4c62a7757e497d4c2aab9e39f67b3a98b612
ash <ashishg@qburst.com>
2018-01-11 18:36:53 +0530

    Throttle persisting state to localStorage to at most one call per second

--- src/app/storage.service.ts
+++ src/app/storage.service.ts
@@ -2,18 +2,21 @@ import { Injectable } from '@angular/core';
 
 import { of } from 'rxjs/observable/of';
 import { Observable } from 'rxjs/Observable';
+import { throttle } from 'lodash'
 
 import { TodoAppState } from './state'
 import { store } from './store'
 import { loadState, saveState } from './storageHandler'
 
+
+
 @Injectable()
 export class StorageService {
 
   constructor() {
-    store.subscribe(() => {
+    store.subscribe(throttle(() => {
       saveState(store.getState());
-    });
+    }, 1000));
   }
 
   getItems = (): Observable<TodoAppState> => { return of(loadState()); }
